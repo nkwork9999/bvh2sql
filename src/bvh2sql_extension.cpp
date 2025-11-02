79477d6d1b7916f8f002352c230f43917141b1b2
nk <nkwork1234@gmail.com>
2025-11-03 02:43:03 +0900

    Fix M_PI undefined error on Windows

--- src/bvh2sql_extension.cpp
+++ src/bvh2sql_extension.cpp
@@ -16,6 +16,10 @@
 #include <cctype>
 #include <cmath>
 
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
+
 namespace bvh_absolute {
 
 // =============================================================================
