857e1042a180de4ad38ddc35d6fe912ef6f9a31e
nk <nkwork1234@gmail.com>
2026-06-12 20:05:32 +0900

    Fix Windows build C++ standard

--- extension_config.cmake
+++ extension_config.cmake
@@ -1,5 +1,12 @@
 # This file is included by DuckDB's build system. It specifies which extension to load
 
+# DuckDB v1.5.x still defaults to C++11 in CMake, but its bundled fmt needs
+# C++17 with current MSVC. Force the global standard before third_party targets
+# such as duckdb_fmt are created.
+set(CMAKE_CXX_STANDARD 17 CACHE STRING "C++ standard to enforce" FORCE)
+set(CMAKE_CXX_STANDARD_REQUIRED ON)
+set(CMAKE_CXX_EXTENSIONS OFF)
+
 # Extension from this repo
 duckdb_extension_load(bvh2sql
     SOURCE_DIR ${CMAKE_CURRENT_LIST_DIR}
