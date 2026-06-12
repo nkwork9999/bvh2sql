3165db705c974ccdc85430d3f6f901bd34b72e71
nk <nkwork1234@gmail.com>
2026-06-12 20:38:20 +0900

    Fix MSVC secure iterator build

--- extension_config.cmake
+++ extension_config.cmake
@@ -7,6 +7,12 @@ set(CMAKE_CXX_STANDARD 17 CACHE STRING "C++ standard to enforce" FORCE)
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
 set(CMAKE_CXX_EXTENSIONS OFF)
 
+# VS 2026 no longer provides stdext::checked_array_iterator, but the fmt copy
+# bundled with DuckDB v1.5.3 still references it when _SECURE_SCL is defined.
+if(MSVC)
+    add_compile_options($<$<COMPILE_LANGUAGE:CXX>:/U_SECURE_SCL>)
+endif()
+
 # Extension from this repo
 duckdb_extension_load(bvh2sql
     SOURCE_DIR ${CMAKE_CURRENT_LIST_DIR}
